1da48b670e70c94c33ba9a23df9da80389706a60
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-02-01 14:56:38 +0530

    Time: 3 ms (83.39%), Space: 7.2 MB (43.04%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        return (str1 + str2 == str2 + str1) ? str2.substr(0,gcd(str1.size(),str2.size())) : "";
+    
+        if(str1 + str2 == str2 + str1){
+            return str2.substr(0,gcd(str1.size(),str2.size()));
+        }
+        else{
+            return "";
+        }
     }
 };
