7ef1ea510f294b241a13fb72f503e48b24371db5
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-02-01 23:54:48 +0530

    Time: 7 ms (33.75%), Space: 7.5 MB (92.41%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,24 @@
 class Solution {
-            private: void permu(vector<int>& ds,vector<int>& nums, vector<vector<int>>& ans,int freq[]){
-                if(ds.size()==nums.size()){
-                    ans.push_back(ds);
-                    return;
-                }
-                for(int i=0;i<nums.size();i++){
-                    if(!freq[i]){
-                    ds.push_back(nums[i]);
-                        freq[i]=1;
-                        permu(ds,nums,ans,freq);
-                        freq[i]=0;
-                        ds.pop_back();
+                private: void permu(int ind,vector<int>& nums, vector<vector<int>>& ans){
+                    if(ind==nums.size()){
+                        ans.push_back(nums);
+                        return;
                     }
+                    for(int i=ind;i<nums.size();i++){
+
+                      swap(nums[ind],nums[i]);
+                        permu(ind+1,nums,ans);
+                         swap(nums[ind],nums[i]);
 
                 }
             }
     
 public:
         vector<vector<int>> permute(vector<int>& nums) {
-            vector<vector<int>> ans;
-            vector<int> ds;
-            int freq[nums.size()];
-            for(int i=0;i<nums.size();i++) freq[i]=0;
-            permu(ds,nums,ans,freq);
-            return ans;
-        
+            
+                vector<vector<int>> ans;
+                permu(0,nums,ans);
+                return ans;
+
     }
 };
