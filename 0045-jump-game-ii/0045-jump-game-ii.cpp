c702ff980929bdd6fdccc68e6fe53638a14f5a23
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-02-08 13:33:52 +0530

    Time: 8 ms (98.04%), Space: 16.5 MB (84.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps=0,curr=0,far=0;
+        
+        for(int i=0;i<nums.size()-1;i++) {
+            
+            far= max(far,nums[i]+i);
+            
+            if(i==curr){
+                curr=far;
+                jumps++;
+                
+            }
+        }
+         return jumps;
+        
+    }
+};
