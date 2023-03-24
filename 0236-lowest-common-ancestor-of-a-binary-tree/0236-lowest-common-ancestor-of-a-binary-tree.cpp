9bc4ba7c400b8906bed46daa79bc4998cf798053
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-03-24 21:52:49 +0530

    Time: 14 ms (86.58%), Space: 14.3 MB (63.60%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return NULL;
+        if(root==p || root==q)
+            return root;
+        TreeNode* LCA1=lowestCommonAncestor(root->left,p,q);
+        TreeNode* LCA2=lowestCommonAncestor(root->right,p,q);
+         
+        if(LCA1!=NULL && LCA2!=NULL)
+            return root;
+        if(LCA1!=NULL)
+            return LCA1;
+        else
+            return LCA2;
+    }
+};
