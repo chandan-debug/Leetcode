76f701cd2dad2c8c7820e085871d39fc348da8fa
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-04-01 20:13:03 +0530

    Time: 23 ms (96.55%), Space: 58 MB (31.79%) - LeetHub

---
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    TreeNode* first;
+        TreeNode* prev;
+        TreeNode* middle;
+        TreeNode* last;
+    private:
+    void inorder(TreeNode* root){
+        if(root==NULL)
+            return;
+        inorder(root->left);
+        
+    if(prev !=NULL && (root->val <prev->val)){
+        //if the first voilation mark these two node as
+        //first nad middle
+        
+        if(first==NULL){
+            first=prev;
+            middle=root;
+        }
+        // if the 2nd voilation mark this node as last 
+        else{
+            last=root;
+        }
+        // mark this node as previous
+    }
+        prev=root;
+        inorder(root->right);
+    }
+
+
+public:
+    void recoverTree(TreeNode* root) {
+        first=middle=last=NULL;
+        prev=new TreeNode(INT_MIN);
+        inorder(root);
+        if(first && last)
+            swap(first->val ,last->val);
+        else if(first && middle)
+            swap(first->val ,middle->val);
+        
+    }
+};
