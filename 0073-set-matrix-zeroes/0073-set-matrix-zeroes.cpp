d0355faf05862492273bc293af44e1f096db9435
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-05-19 20:36:10 +0530

    Time: 21 ms (18.97%), Space: 13.4 MB (35.14%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        
-        vector<int> rowArray(n,1),colArray(m,1);
-        
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    rowArray[i]=0;
-                    colArray[j]=0;
-                }
+   void setZeroes(vector<vector<int>>& matrix) {
+    int n = matrix.size();  // Number of rows
+    int m = matrix[0].size();  // Number of columns
+
+    vector<int> rowArray(n, 1);  // Initialize a vector to track zero rows
+    vector<int> colArray(m, 1);  // Initialize a vector to track zero columns
+
+    // Step 1: Traverse the matrix and mark the zero rows and columns
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                rowArray[i] = 0;  // Mark the row as zero
+                colArray[j] = 0;  // Mark the column as zero
             }
-        
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++){
-                if(colArray[j]==0 || rowArray[i]==0) {
-                    matrix[i][j]=0;
-                }
-            }
+        }
     }
+
+    // Step 2: Modify the matrix based on the marked zero rows and columns
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (colArray[j] == 0 || rowArray[i] == 0) {
+                matrix[i][j] = 0;  // Set the element to zero if the row or column is marked as zero
+            }
+        }
+    }
+}
+
 };
