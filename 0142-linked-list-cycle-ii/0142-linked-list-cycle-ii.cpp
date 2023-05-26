d8519b694059ab567a9f802b7b6f00eeecfc2980
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-05-26 16:04:23 +0530

    Time: 12 ms (41.35%), Space: 7.5 MB (98.49%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -24,7 +24,7 @@ public:
                 slow=slow->next;
                 entry=entry->next;
                 }
-                 return slow;
+                 return entry;
             }
            
         }
