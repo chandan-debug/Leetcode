597a7f591ec4cb4531c205f2cd117b2848c0f763
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-06-02 11:35:25 +0530

    Time: 3 ms (69.29%), Space: 7.6 MB (69.68%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,22 @@
 class Solution {
-                private: void permu(int ind,vector<int>& nums, vector<vector<int>>& ans){
-                    if(ind==nums.size()){
-                        ans.push_back(nums);
-                        return;
-                    }
-                    for(int i=ind;i<nums.size();i++){
-
-                      swap(nums[ind],nums[i]);
-                        permu(ind+1,nums,ans);
-                         swap(nums[ind],nums[i]);
-
-                }
-            }
-    
 public:
-        vector<vector<int>> permute(vector<int>& nums) {
-            
-                vector<vector<int>> ans;
-                permu(0,nums,ans);
-                return ans;
-
+    void func(int ind, vector<int>& nums,vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            func(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        
+        func(0,nums,ans);
+        return ans;
+        
     }
 };
