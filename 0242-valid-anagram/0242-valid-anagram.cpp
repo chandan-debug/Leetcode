959236d7b005d643953ea4bbaef88e4a24189d4b
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-06-10 14:42:12 +0530

    Time: 9 ms (74.33%), Space: 7.3 MB (81.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        vector<int> count(26, 0); // Character count array
+        
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++; // Increment count for characters in s
+            count[t[i] - 'a']--; // Decrement count for characters in t
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false; // If any count is non-zero, strings are not anagrams
+            }
+        }
+        
+        return true; // All counts are zero, strings are anagrams
     }
 };
