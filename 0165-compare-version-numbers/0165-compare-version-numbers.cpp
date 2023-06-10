b684cbdff15f9f9eafa965a2d61af6a4d2f9759e
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-06-10 16:20:51 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.40%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int i=0,j=0;
+        int version1len=version1.size(),version2len=version2.size(),n1,n2;
+        while(i<version1len || j<version2len){
+            n1=0;
+            n2=0;
+            while(i<version1len && version1[i]!='.'){
+                n1=n1*10+(version1[i]-'0');
+                i++;
+            }
+            while(j<version2len && version2[j]!='.'){
+                n2=n2*10+(version2[j]-'0');
+                j++;
+            }
+            if(n1<n2)
+                return -1;
+            else if(n1>n2)
+                return 1;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+};
