e63c937e704e1d872643a0724e40f411e8535e2d
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-07-09 11:30:07 +0530

    Time: 406 ms (22.01%), Space: 6.9 MB (62.26%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -0,0 +1,36 @@
+/*Approach
+iterate through every possible pair of character and finding the varance and maximum among them will be answer.for this we will always take c1 greater than c2 so answer is postive.automatically the case will be covered for c2 greater than c1 when j and i character swap its posistion.*/
+class Solution {
+public:
+    int largestVariance(string s) {
+        vector<int>arr(26);
+        for(auto x:s){
+           arr[x-'a']++; 
+        }
+        int ans=0;
+        for(char i='a';i<='z';i++){
+            for(char j='a';j<='z';j++){
+                 if(j==i||arr[i-'a']==0||arr[j-'a']==0)
+                            continue;
+                for(int k=1;k<=2;k++){
+                   int c1=0;
+                    int c2=0;
+                    for(auto x:s){
+                        if(x==i)
+                            c1++;
+                        if(x==j)
+                            c2++;
+                        if(c2>c1){
+                            c1=0;
+                            c2=0;
+                        }
+                        if(c1>0&&c2>0)
+                        ans=max(ans,c1-c2);
+                    }
+                    reverse(s.begin(),s.end());
+                }
+            }
+        }
+        return ans;
+    }
+};
