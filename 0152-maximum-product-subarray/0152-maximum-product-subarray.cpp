08c6d03c3bb53d80a8a158102b4923c89da4aec9
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-07-24 12:03:04 +0530

    Time: 12 ms (25.90%), Space: 13.7 MB (60.90%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,8 +6,8 @@ public:
         int min_prod = nums[0];
 
         for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < 0)
-                swap(max_prod, min_prod);
+            if (nums[i] < 0){
+                swap(max_prod, min_prod);}
 
             max_prod = max(nums[i], max_prod * nums[i]);
             min_prod = min(nums[i], min_prod * nums[i]);
