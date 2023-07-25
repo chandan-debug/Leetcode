0951a8e71455d61714ca0b70f60e8a3d05a7e75c
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-07-25 18:46:08 +0530

    Time: 134 ms (28.36%), Space: 59.7 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,15 +1,39 @@
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int largestelement=INT_MIN;
+//         int ans=-1;
+//         for(int i=0;i<arr.size();i++){
+//             if(arr[i]>largestelement){
+//                 ans=i;
+//                 largestelement=arr[i];
+//             }
+//         }
+//         return ans;
+        
+//     } TC=O(arr.size())
+//Sc=O(1)
+// };
+
+
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int largestelement=INT_MIN;
-        int ans=-1;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]>largestelement){
-                ans=i;
-                largestelement=arr[i];
+        int left=0;
+        int right=arr.size()-1;
+        
+        while(left<right){
+            int mid=left+(right-left)/2;
+            
+            if(arr[mid]<arr[mid+1]){
+                left=mid+1;
+            }
+            else{
+                right=mid;
             }
         }
-        return ans;
+        return left;
+        
         
     }
 };
