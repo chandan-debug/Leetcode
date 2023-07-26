458280fdc07a3edd7f20af04303e024a4b552c17
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-07-26 14:20:44 +0530

    Time: 518 ms (10.65%), Space: 101.5 MB (67.19%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        if((double) dist.size()-1>=hour)
+            return -1;
+        int left=1;
+        int right=(int)10e7;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(condition(mid,dist,hour)){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
+        
+    }
+    public : bool condition(int s,vector<int>&dist,double hour){
+        double time=0;
+        for(int i=0;i<dist.size()-1;i++){
+            time+=dist[i]/s;
+            time+=(dist[i]%s==0)?0:1;
+        }
+        time+=dist[dist.size()-1]*1.0/s;
+        return time<=hour;
+    }
+};
