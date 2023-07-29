fde94c9b19aa55e599863c380fbdbb3d6b3b530e
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-07-29 14:47:54 +0530

    Time: 11 ms (50.57%), Space: 138.3 MB (31.56%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    vector<vector<double>> dp;
-    double takesoup(int a, int b){
+    
+    double takesoup(int a, int b,vector<vector<double>>& dp){
         if(a<=0 && b>0) return 1.0;
         if(a<=0 && b<=0) return 0.5;
         if(a>0 && b<=0) return 0.0;
 
         if(dp[a][b] != -1) return dp[a][b];
 
-        return dp[a][b] = 0.25*(takesoup(a-100,b) + takesoup(a-75,b-25) + takesoup(a-50,b-50) + takesoup(a-25,b-75));
+        return dp[a][b] = 0.25*(takesoup(a-100,b,dp) + takesoup(a-75,b-25,dp) + takesoup(a-50,b-50,dp) + takesoup(a-25,b-75,dp));
     }
     double soupServings(int n) {
         if(n >= 5000) return 1.0;
-        dp.resize(n+1,vector<double>(n+1,-1));
-        return takesoup(n,n);
+        vector<vector<double>> dp(n+1,vector<double>(n+1,-1));
+        return takesoup(n,n,dp);
     }
 };
