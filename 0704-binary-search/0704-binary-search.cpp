fad9ffa9ea225ea3be504db9726d9363fa035caf
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-02 15:46:51 +0530

    Time: 24 ms (98.83%), Space: 27.6 MB (31.73%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-   int search(vector<int>& arr,int s,int e, int key){
-    //basecase
-    //elemnt not found
-        if(s>e){
-            return -1;
-        }
-        int mid=(s+e)>>1;
-    //element found
-        if(arr[mid]==key){
-            return mid;
-        }
-
-        if(arr[mid]<key){
-            return search(arr,mid+1,e,key);
-        }
-        else{
-            return search(arr,s,mid-1,key);
-        }
-}
     int search(vector<int>& nums, int target) {
-        return search(nums,0,nums.size()-1,target);
+        int start=0;
+        int end=nums.size()-1;
+        
+        
+        while(start<=end)
+        {
+           int mid=start+(end-start)/2;
+            if(target==nums[mid])
+            {
+                return mid;
+            } 
+            else if(target<nums[mid])
+            {
+                end=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                start=mid+1;
+            }
+        }
+        return -1;
+
     }
 };
-
-
- 
-
