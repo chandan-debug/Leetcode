ed22f18a0037324ca0f3f5dc1ea3a77ff1adf384
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-02 10:39:19 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (59.46%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,12 +12,13 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL || q == NULL) {
-            return (p == q);
-        }
+        if(p==NULL && q==NULL)
+            return true;
         
-        return (p->val == q->val)
-            && isSameTree(p->left, q->left)
-            && isSameTree(p->right, q->right);
+        if(p==NULL || q==NULL || p->val!=q->val)
+            return false;
+        
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        
     }
 };
