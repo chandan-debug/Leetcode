8e5d44fda400073f961352f1ec175f59130da8a3
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-03 13:58:41 +0530

    Time: 146 ms (61.42%), Space: 73.4 MB (7.56%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        bool flag = false;
-        for(int i =0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]) return true;
+        map<int,int> mp;
+        for(auto i:nums) mp[i]++;
+        bool flag=false;
+        for(auto i:mp){
+            if(i.second>=2)
+                return true;
         }
         return flag;
     }
