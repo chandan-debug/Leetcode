1a9c8dd5729889c9e3d0721f6c1d81c4e3d4cac4
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-04 11:36:20 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (36.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        stringstream ss(s);
-    string word;
-    while (ss >> word) {}
-    return word.length();
+        int count=0,flag=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' '&&flag)break;
+            if(s[i]!=' '){
+                flag=1;
+                count++;
+            }
+        }
+        return count;
     }
 };
