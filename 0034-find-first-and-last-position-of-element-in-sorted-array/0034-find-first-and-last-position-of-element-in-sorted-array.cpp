fade86f5c4c5f0c75d81792008cd1e057cc1f620
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-04 11:14:04 +0530

    Time: 7 ms (80.95%), Space: 13.6 MB (28.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,46 @@
 class Solution {
+    public:
+    int firstsearch(vector<int>& nums,int target,int l,int h){
+        int start=-1;
+        while(l<=h){
+            int mid=(l+h)>>1;
+            if(nums[mid]==target){
+                start=mid;
+                h=mid-1;
+            }
+            if(target>nums[mid]){
+                l=mid+1;
+            }
+            if(target<nums[mid]){
+                h=mid-1;
+            }
+        }
+        return start;
+    }
+     int lastsearch(vector<int>& nums,int target,int l,int h){
+        int end=-1;
+        while(l<=h){
+            int mid=(l+h)>>1;
+            if(nums[mid]==target){
+                end=mid;
+                l=mid+1;
+            }
+            if(target>nums[mid]){
+                l=mid+1;
+            }
+            if(target<nums[mid]){
+                h=mid-1;
+            }
+        }
+        return end;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int startingPosition = findStartingPosition(nums, target, 0, n - 1);
-        int endingPosition = findEndingPosition(nums, target, 0, n - 1);
-        return {startingPosition, endingPosition};
-    }
-    
-private:
-    int findStartingPosition(vector<int>& nums, int target, int left, int right) {
-        int startingPosition = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-            if (nums[mid] == target) {
-                startingPosition = mid;
-            }
-        }
-        return startingPosition;
-    }
-    
-    int findEndingPosition(vector<int>& nums, int target, int left, int right) {
-        int endingPosition = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-            if (nums[mid] == target) {
-                endingPosition = mid;
-            }
-        }
-        return endingPosition;
+        int start=firstsearch(nums,target,0,nums.size()-1);
+       int end=lastsearch(nums,target,0,nums.size()-1);
+        return {start,end};
+
+        
+        
     }
 };
