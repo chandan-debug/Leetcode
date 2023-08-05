6a56dce18de1f71722648e4dfbe37100162a0a70
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-05 19:32:25 +0530

    Time: 8 ms (97.28%), Space: 13.3 MB (64.43%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,22 @@
+// My idea is simple: store states of each row in the first of that row, and store states of each column in the first of that column. Because the state of row0 and the state of column0 would occupy the same cell, I let it be the state of row0, and use another variable "col0" for column0. In the first phase, use matrix elements to set states in a top-down way. In the second phase, use states to set matrix elements in a bottom-up way.
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size();  // no.of row
-        int m=matrix[0].size();  // no. of col
-        vector<int> rowarray(n,1);
-        vector<int> colarray(m,1);
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    rowarray[i]=0;
-                    colarray[j]=0;
-                }
-            }
+        int col0 = 1, n = matrix.size(), m = matrix[0].size();
+
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < m; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(rowarray[i]==0 || colarray[j]==0){
-                    matrix[i][j]=0;
-                }
-            }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
         }
-        
     }
 };
