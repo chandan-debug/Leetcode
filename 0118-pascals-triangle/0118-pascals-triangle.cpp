600d43494b5421668e1eea83cdff7ab0ac821166
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-06 15:17:20 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (71.84%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-   vector<vector<int>> generate(int numRows) {
-    vector<vector<int>> r(numRows);  // Initialize a 2D vector to store the triangle
-
-    // Step 1: Iterate over the number of rows
-    for (int i = 0; i < numRows; i++) {
-        r[i].resize(i + 1);  // Resize the current row vector to have (i+1) elements
-        r[i][0] = r[i][i] = 1;  // Set the first and last elements of the row to 1
-
-        // Step 2: Calculate the values for the current row
-        for (int j = 1; j < i; j++) {
-            r[i][j] = r[i - 1][j - 1] + r[i - 1][j];  // Calculate the current element by summing the two elements above it
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>v;
+        for (int i=0; i<numRows; i++) {
+            vector<int>v1(i+1, 1);
+            for (int j=1; j<i; j++) {
+                v1[j]=v[i-1][j]+v[i-1][j-1];
+            }
+            v.push_back(v1);
         }
+        return v;
     }
-
-    return r;  // Return the generated Pascal's triangle
-}
-
 };
