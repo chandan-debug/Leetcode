968a43c58dd674a660c951c9d70354e2de5e33a1
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-07 13:42:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (57.36%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double ans=1.0;
-        long nn=n;
-        if(nn<0){
-            nn=-1*nn;
+        long long a = abs((long long)n);  // Use long long to handle potential overflow
+        double ans = 1.0;
+        
+        while (a > 0) {
+            if (a % 2 == 1)
+                ans *= x;
+            x *= x;
+            a /= 2;
         }
-        while(nn>0){
-            if(nn%2==1){
-                ans=ans*x;
-                nn=nn-1;
-            }
-            else{
-                x=x*x;
-                nn=nn/2;
-            }
-        }
-        if(n<0){
-            ans=(double)(1.0)/(double)(ans);
-            
-        }
-        return ans;
+        
+        return (n < 0) ? 1 / ans : ans;
     }
 };
