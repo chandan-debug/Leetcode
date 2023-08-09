381df6ea5d423c3cb45ec475bbf9ba53d1923c81
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-09 15:08:46 +0530

    Time: 13 ms (89.00%), Space: 6.8 MB (97.02%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,74 @@
 class Solution {
 public:
-    string longestPalindrome(string S) {
-        int start = 0;
-        int maxLen = 1;
-        for (int i = 1; i < S.length(); i++) {
-            // even
-            int l = i - 1;
-            int r = i;
-            while (l >= 0 && r < S.length() && S[l] == S[r]) {
-                if (r - l + 1 > maxLen) {
-                    maxLen = r - l + 1;
-                    start = l;
+
+    string longestPalindrome(string s) {
+        
+        // input string's length
+        int n = s.length();
+
+        // starting index of substring
+        // there is always present a substring of length 1
+        int start = 0 , maxLen = 1;
+
+
+        // traversing the string 
+        // from current index we will go left & right
+        // then check characters are matching or not on left & right
+        for(int i=1;i<n;i++){
+
+            // we will check for odd length substring
+            int left = i , right = i;
+
+            // if we are not out bound & character are matching 
+            // and we will calculate current length
+            // and compare with longest length 
+            while(left >= 0 && right < n && s[left] == s[right]){
+                
+                // current length
+                int currLen = right - left + 1;
+
+                // if current length is greater than maximum Length 
+                // we will we will assign current length to maxLen
+                // and change starting index of longest substring
+                if(currLen > maxLen){
+                    maxLen = currLen;
+                    start = left;
                 }
-                l--;
-                r++;
+
+                // move pointers to left and right
+                left--; right++;
+
             }
-            // odd
-            l = i - 1;
-            r = i + 1;
-            while (l >= 0 && r < S.length() && S[l] == S[r]) {
-                if (r - l + 1 > maxLen) {
-                    maxLen = r - l + 1;
-                    start = l;
+
+
+            // we will check for even length substring
+            left = i-1 , right = i;
+
+            // if we are not out bound & character are matching 
+            // and we will calculate current length
+            // and compare with longest length 
+            while(left >= 0 && right < n && s[left] == s[right]){
+                
+                // current length
+                int currLen = right - left + 1;
+
+                // if current length is greater than maximum Length 
+                // we will we will assign current length to maxLen
+                // and change starting index of longest substring
+                if(currLen > maxLen){
+                    maxLen = currLen;
+                    start = left;
                 }
-                l--;
-                r++;
+
+                // move pointers to left and right
+                left--; right++;
+
             }
+
         }
-        return S.substr(start, maxLen);
+
+        // return longest substring
+        return s.substr(start,maxLen);
+
     }
 };
