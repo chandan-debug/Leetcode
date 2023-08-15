90f28236fd5beb3994c8c9e57bba9cf8bd30bce3
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-15 11:10:48 +0530

    Time: 9 ms (50.83%), Space: 6.5 MB (75.61%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string  ans="1";
-        for(int i=2;i<=n;i++){
-            string laststring=ans;
-            int l=ans.size();
-            int j=0;
-            ans="";
-            while(j<l){
-                int count=j;
-                while(j<l && laststring[j]==laststring[count]){
-                    count++;
-                }
-                ans+=to_string(count-j)+laststring[j];
-                j=count;
+      if(n==1) return "1";                       //if n==1, return "1"
+    if(n==2) return "11";                      //if n==2, return "11"
+    
+    string s="11";                             //initialise s = "11"
+    
+    for(int i=3;i<=n;i++){
+        
+        string temp="";                        //empty string, temp
+        s+= '$';                               //append a random character, therefore, s = "11$"
+        int count=1;                           //initially count = 1
+        
+        for(int j=1;j<s.size();j++){           //traverse string s from idx 1 to s.size()
+            
+            if(s[j]!=s[j-1]){                  //if curr_character and prev_character doesn't match
+                
+                temp+= to_string(count);       //First,  add count to temp string
+                temp+= s[j-1];                 //Second, add the character (prev_character)
+                count=1;                       //Third,  reset the count
             }
+            else count++;                      //if curr_character and prev_character match, increment count
+            
         }
-        return ans;
+        
+        s=temp;                                //Update string s wuth temp
     }
+    
+    return s;                                  //return s
+}
 };
