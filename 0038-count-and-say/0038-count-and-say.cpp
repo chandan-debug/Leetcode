9e0cccd7ea4013ce375dca0b036bdda23813fa0e
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-15 11:26:27 +0530

    Time: 6 ms (73.75%), Space: 6.7 MB (57.26%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     string countAndSay(int n) {
-      if(n==1) return "1";                       //if n==1, return "1"
-    if(n==2) return "11";                      //if n==2, return "11"
-    
-    string s="11";                             //initialise s = "11"
-    
-    for(int i=3;i<=n;i++){
+        if(n==1) return "1";
+        if(n==2) return "11";
         
-        string temp="";                        //empty string, temp
-        s+= '$';                               //append a random character, therefore, s = "11$"
-        int count=1;                           //initially count = 1
+        string s="11";
         
-        for(int j=1;j<s.size();j++){           //traverse string s from idx 1 to s.size()
-            
-            if(s[j]!=s[j-1]){                  //if curr_character and prev_character doesn't match
-                
-                temp+= to_string(count);       //First,  add count to temp string
-                temp+= s[j-1];                 //Second, add the character (prev_character)
-                count=1;                       //Third,  reset the count
+        for(int i=3;i<=n;i++){
+            string temp="";
+            s+='$';
+            int cnt=1;
+            for(int j=1;j<s.size();j++){
+                if(s[j]!=s[j-1]){
+                    temp+=to_string(cnt);
+                    temp+=s[j-1];
+                    cnt=1;
+                }
+                else{
+                    cnt++;
+                }
             }
-            else count++;                      //if curr_character and prev_character match, increment count
-            
+            s=temp;
         }
-        
-        s=temp;                                //Update string s wuth temp
+        return s;
     }
-    
-    return s;                                  //return s
-}
 };
