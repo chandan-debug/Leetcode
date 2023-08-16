d63d5a68c6ac6d5a2a8ac961b0c50efb6f0282f5
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-16 19:01:40 +0530

    Time: 217 ms (82.15%), Space: 134.7 MB (66.10%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,14 +4,14 @@ public:
         deque<int> dq;
         vector<int> ans;
         for(int i=0;i<nums.size();i++){
-            if(!dq.empty() && dq.front()==i-k)
+            if(!dq.empty() && dq.front()==i-k)  //out of bound
                 dq.pop_front();
-           while(!dq.empty() && nums[dq.back()] < nums[i]){
+           while(!dq.empty() && nums[dq.back()] < nums[i]){  //check decreasing fashion 
                 dq.pop_back();
            }
                 dq.push_back(i);
             
-            if(i>=k-1)
+            if(i>=k-1)  // indx =0,1,2  and k=3
               ans.push_back(nums[dq.front()]);
         }
         return ans;
