f2378e4e24ccf8212d18d558b1ac0f85df8a13ff
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-18 12:36:34 +0530

    Time: 7 ms (31.65%), Space: 10.8 MB (13.46%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,22 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* dummy = new ListNode();
-        dummy->next = head;
-        ListNode* slow = dummy;
-        ListNode* fast = dummy;
+        ListNode* start=new ListNode();
+        start->next=head;
+        ListNode* fast =start;
+        ListNode* slow=start;
 
-        // Move fast pointer n+1 steps ahead
-        for (int i = 0; i <= n; i++) {
-            fast = fast->next;
+        for(int i=1; i<=n; ++i )
+            fast=fast->next;
+
+        while(fast->next != NULL)
+        {
+            fast=fast->next;
+            slow=slow->next;
         }
-
-        // Move fast to the end, maintaining the gap of n between slow and fast
-        while (fast != nullptr) {
-            fast = fast->next;
-            slow = slow->next;
-        }
-
-        // Remove the Nth node from the end
-        ListNode* toDelete = slow->next;
-        slow->next = slow->next->next;
-        delete toDelete;
-
-        return dummy->next;
+        slow->next=slow->next->next;
+        
+        
+        return start->next;
     }
 };
