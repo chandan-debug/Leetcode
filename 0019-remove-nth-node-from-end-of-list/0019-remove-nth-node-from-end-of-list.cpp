d6394c204509204fb11a454d5c9f071ed2a56fa6
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-18 12:35:17 +0530

    Time: 7 ms (31.65%), Space: 10.7 MB (45.70%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,23 +1,37 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* start=new ListNode();
-        start->next=head;
-        ListNode* fast =start;
-        ListNode* slow=start;
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        ListNode* slow = dummy;
+        ListNode* fast = dummy;
 
-        for(int i=1; i<=n; ++i )
-            fast=fast->next;
-
-        while(fast->next != NULL)
-        {
-            fast=fast->next;
-            slow=slow->next;
+        // Move fast pointer n+1 steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast->next;
         }
-        slow->next=slow->next->next;
-        
-        
-        return start->next;
+
+        // Move fast to the end, maintaining the gap of n between slow and fast
+        while (fast != nullptr) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // Remove the Nth node from the end
+        ListNode* toDelete = slow->next;
+        slow->next = slow->next->next;
+        delete toDelete;
+
+        return dummy->next;
     }
 };
