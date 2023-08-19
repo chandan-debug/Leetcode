a8559ad633bc5382f0c7e12ed13a641301d89119
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-19 12:05:44 +0530

    Time: 4 ms (83.51%), Space: 14.8 MB (24.69%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,23 +11,27 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL)
-            return list2;
-        if(list2==NULL)
-            return list1;
-        if(list1->val>list2->val)
-            swap(list1,list2);
-        ListNode* res=list1;
-        while(list1!=NULL && list2!=NULL){
-            ListNode* tmp=NULL;
-            while(list1!=NULL && list1->val <= list2->val){
-                tmp=list1;
-                list1=list1->next;
+        ListNode dummy(0);  // Dummy node to simplify the code
+        ListNode* prev = &dummy;
+        
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                prev->next = list1;
+                list1 = list1->next;
+            } else {
+                prev->next = list2;
+                list2 = list2->next;
             }
-            tmp->next=list2;
-            swap(list1,list2);
+            prev = prev->next;
         }
         
-        return res;
+        // Attach the remaining nodes (if any) from either list
+        if (list1 != NULL) {
+            prev->next = list1;
+        } else {
+            prev->next = list2;
+        }
+        
+        return dummy.next;  // Return the merged list starting from the next of dummy node
     }
 };
