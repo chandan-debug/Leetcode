550e2cf309758dd88669d6fbdcdb1fad5960d96e
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-25 11:08:17 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (38.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for(auto c:s){
-        if(st.empty()){
-            st.push(c);
-        }
-        else if((st.top()=='(' && c==')') || (st.top()=='{' && c=='}') || (st.top()=='[' && c==']')){
-            st.pop();
-        }
-            else{
+        
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (st.empty()) {
+                st.push(c);
+            } else if ((st.top() == '(' && c == ')') || (st.top() == '{' && c == '}') || (st.top() == '[' && c == ']')) {
+                st.pop();
+            } else {
                 st.push(c);
             }
-            }
-        if(st.size()==0){
+        }
+        
+        if (st.empty()) {
             return true;
         }
+        
         return false;
     }
 };
