5b90999c2aea23705e4568b83dd9f134cb905e39
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-26 18:55:20 +0530

    Time: 197 ms (73.86%), Space: 84.1 MB (42.25%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,29 +1,30 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>> st;
-    int index;
+    stack<pair<int, int>> st;  // Stack to store pairs of (day index, stock price)
+    int index;  // Current day index
+    
     StockSpanner() {
-        index=-1;
+        index = -1;  // Initialize the current day index
     }
     
     int next(int price) {
-        index+=1;
-        while(!st.empty() && price>=st.top().second){
+        index += 1;  // Move to the next day
+        
+        // Pop elements from stack while the current price is greater than or equal to the price on the stack top
+        while (!st.empty() && price >= st.top().second) {
             st.pop();
         }
-        if(st.empty()){
-            st.push({index,price});
-            return index+1;
-        }
-        int curr=st.top().first;
-        st.push({index,price});
-        return index-curr;
         
+        if (st.empty()) {
+            // If stack is empty, the current day's price is the highest encountered so far
+            st.push({index, price});
+            return index + 1;  // Span is the current index + 1
+        }
+        
+        int curr = st.top().first;  // Get the index of the most recent day with a smaller price
+        st.push({index, price});    // Push the current day's index and price onto the stack
+        
+        // Span is the difference between the current index and the index of the most recent smaller price day
+        return index - curr;
     }
 };
-
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
- */
