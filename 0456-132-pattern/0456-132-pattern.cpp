91ff5b7bd0943d5f7e3be73cdf7e13c59c7b54bb
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-27 10:53:40 +0530

    Time: 67 ms (79.48%), Space: 48.6 MB (38.93%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        stack<int> st;         // Initialize a stack to store elements
-        int s = INT_MIN;       // Initialize a variable to track the potential third element
-        int n = nums.size();   // Get the size of the input array
-        
-        // Traverse the array in reverse
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] < s) {
-                return true;   // Check if current element is smaller than potential third element, return true
+        stack<int> st;
+        int s=INT_MIN;
+        int n=nums.size();
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<s){
+                return true;
             }
-            
-            while (!st.empty() && nums[i] > st.top()) { // ex 35034 -> 354
-                                // The while loop is used to update s to the maximum potential third element
-                s = st.top();    // Update s with the value of the top element from the stack
-                st.pop();        // Pop the element from the stack
+            while(!st.empty() && nums[i]>st.top()){
+                s=st.top();
+                st.pop();
             }
-            
-            st.push(nums[i]);   // Push the current element onto the stack for potential comparisons
+            st.push(nums[i]);
         }
-        
-        return false;          // If no pattern is found, return false
+        return false;
     }
 };
