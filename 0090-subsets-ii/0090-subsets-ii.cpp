bd75026d85a495c7211f24b87cd276f6ec2d1c13
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-08-31 07:45:23 +0530

    Time: 4 ms (53.27%), Space: 7.4 MB (93.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     void solve(int ind, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans) {
-        ans.push_back(ds);  // Include the current subset
+        
         
         for (int i = ind; i < nums.size(); i++) {
             if (i != ind && nums[i] == nums[i - 1]) {
@@ -11,6 +11,7 @@ private:
             solve(i + 1, nums, ds, ans);
             ds.pop_back();
         }
+        ans.push_back(ds);  // Include the current subset
     }
     
 public:
