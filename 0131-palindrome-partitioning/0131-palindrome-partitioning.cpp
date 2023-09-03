df45ff9e25a94149338fb01a8b3eac70b516df47
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-03 20:32:01 +0530

    Time: 97 ms (78.24%), Space: 57.6 MB (41.59%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    public:
+   public:
     bool ispalin(string s, int start, int end){
         while(start<=end){
             if(s[start++]!=s[end--]){
@@ -9,17 +9,15 @@ class Solution {
         return true;
     }
     
-    
-    void func(int ind, string& s,vector<string>& ds,vector<vector<string>>& ans){
-        if(ind==s.size()){
+    void solve(int i,string s ,vector<string>& ds, vector<vector<string>>& ans){
+        if(i==s.size()){
             ans.push_back(ds);
             return;
         }
-        
-        for(int i=ind;i<s.size();++i){
-            if(ispalin(s,ind,i)){
-                ds.push_back(s.substr(ind,i-ind+1));
-                func(i+1,s,ds,ans);
+        for(int j=i;j<s.size();j++){
+            if(ispalin(s,i,j)){
+                ds.push_back(s.substr(i,j-i+1));
+                solve(j+1,s,ds,ans);
                 ds.pop_back();
             }
         }
@@ -28,8 +26,8 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> ds;
-        func(0,s,ds,ans);
-        
+        solve(0,s,ds,ans);
         return ans;
+        
     }
 };
