834d35bbbc6267eaf4b6ccc97b275ce13e3afc2a
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-04 14:48:48 +0530

    Time: 9 ms (38.75%), Space: 7.1 MB (76.95%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,33 +1,44 @@
 class Solution {
-public:
-    void solve(int col, vector<string>& board,vector<vector<string>>&ans,vector<int>& leftrow,vector<int>&upperdiagonal,vector<int>&lowerdiagonal,int n){
-        if(col==n){ans.push_back(board);
-                  return;}
-        for(int row=0;row<n;row++){
-            if(leftrow[row]==0 && lowerdiagonal[row+col]==0 &&upperdiagonal[n-1+col-row]==0){
-                board[row][col]='Q';
-                leftrow[row]=1;
-                lowerdiagonal[row+col]=1;
-                upperdiagonal[n-1+col-row]=1;
-                solve(col+1,board,ans,leftrow,upperdiagonal,lowerdiagonal,n);
-                board[row][col]='.';
-                leftrow[row]=0;
-                lowerdiagonal[row+col]=0;
-                upperdiagonal[n-1+col-row]=0;
-                
+
+private:
+    bool inbound(int i, int j, int n) {
+        return i >= 0 && i < n && j >= 0 && j < n;
+    }
+
+    bool issafe(int r, int c, const vector<string>& temp, int n) {
+        // Check for row and column
+        for (int k = 0; k < n; k++) {
+            if (temp[k][c] == 'Q' || temp[r][k] == 'Q')
+                return false;
+        }
+        // Check diagonals
+        for (int l = 1; l < n; l++) {
+            if (inbound(r - l, c - l, n) && temp[r - l][c - l] == 'Q') return false;
+            if (inbound(r - l, c + l, n) && temp[r - l][c + l] == 'Q') return false;
+            if (inbound(r + l, c - l, n) && temp[r + l][c - l] == 'Q') return false;
+            if (inbound(r + l, c + l, n) && temp[r + l][c + l] == 'Q') return false;
+        }
+        return true;
+    }
+
+    void solve(int row, int n, vector<string>& temp, vector<vector<string>>& ans) {
+        if (row == n) {
+            ans.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (issafe(row, i, temp, n)) {
+                temp[row][i] = 'Q';
+                solve(row + 1, n, temp, ans);
+                temp[row][i] = '.'; // backtrack
             }
         }
     }
+    public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
-        string s(n,'.');
-        for(int i=0;i<n;i++){
-            board[i]=s;
-        }
-        vector<int> leftrow(n,0),upperdiagonal(2*n-1,0),lowerdiagonal(2*n-1,0);
-        solve(0,board,ans,leftrow,upperdiagonal,lowerdiagonal,n);
+        vector<string> temp(n, string(n, '.')); 
+        solve(0, n, temp, ans);
         return ans;
-        
     }
 };
