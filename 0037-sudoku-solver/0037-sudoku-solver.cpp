9417d1c9c413931369faad636a2ba7ab1c6e6380
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-05 17:31:09 +0530

    Time: 23 ms (74.25%), Space: 6.3 MB (71.01%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,37 +1,61 @@
 class Solution {
 public:
-    bool isvalid(vector<vector<char>>& board,int row,int col,char c){
-        for(int i=0;i<9;i++){
-            if(board[i][col]==c)
-                return false;
-            if(board[row][i]==c)
-                return false;
-            if (board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
-                return false;
+    bool notclash(char num, int i, int j, vector<vector<char>>& board, int n) {
+        for (int k = 0; k < n; k++) {
+            // row
+            if (board[i][k] == num) return false;
+            // column
+            if (board[k][j] == num) return false;
         }
-        return true;
-    }
-    bool solve(vector<vector<char>>& board){
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                if(board[i][j]=='.'){
-                    for(char c='1';c<='9';c++){
-                        if(isvalid(board,i,j,c)){
-                            board[i][j]=c;
-                            if(solve(board)==true)
-                                return true;
-                            else
-                                board[i][j]='.';
-                        }
-                    }
-                    return false;
-                }
+
+        // box
+        int s = sqrt(n);
+        int startrow = i - i % s;
+        int startcol = j - j % s;
+
+        for (int r = startrow; r < startrow + s; r++) {
+            for (int c = startcol; c < startcol + s; c++) {
+                if (board[r][c] == num) return false;
             }
         }
-            return true;
+
+        return true;
+    }
+
+    bool solveboard(std::vector<vector<char>>& board, int n) {
+        bool flag = false;
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                // we have to fill this cell
+                if (board[i][j] == '.') {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) break;
         }
-    
+
+        if (i == n && j == n) return true;
+
+        // i, j -> fill the value
+        for (int num = 1; num <= n; num++) {
+            // convert the number to char
+            char numChar = num + '0';
+
+            // check if not clashing
+            if (notclash(numChar, i, j, board, n)) {
+                board[i][j] = numChar;
+                if (solveboard(board, n)) return true;
+                board[i][j] = '.';
+            }
+        }
+
+        return false;
+    }
+
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        int n = board.size();
+        solveboard(board, n);
     }
 };
