7599c33c32053bad126074ab7b2cd76dce268c26
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-07 14:59:12 +0530

    Time: 6 ms (20.76%), Space: 11.9 MB (62.88%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,21 +11,20 @@
  */
 class Solution {
 public:
-    void solve(TreeNode* root,int level,vector<int>&ans){
+    void solve(TreeNode* root, int level,vector<int>& ans){
         if(root==NULL)
             return;
-        if(ans.size()==level)
-            ans.push_back(root->val);
         
-        solve(root->right,level+1,ans);
-        solve(root->left,level+1,ans);
-       
+        if(ans.size()==level){
+            ans.push_back(root->val);
+        }
+            solve(root->right,level+1,ans);
+            solve(root->left,level+1,ans);
+        
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        solve(root,0,ans);
+         solve(root,0,ans);
         return ans;
-       
-        
     }
 };
