94ed9bce9bb9d97e7f35ed50c6067adbcd90efc4
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-13 09:00:24 +0530

    Time: 4 ms (87.77%), Space: 17.3 MB (82.08%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,41 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == nullptr)
-            return 0;
+        // Check if the tree is empty
+        if (!root) return 0;
 
-        int ans = 0;
-        queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
+        // Create a queue to perform level-order traversal
+        queue<pair<TreeNode*, unsigned long long>> q;  // Use unsigned long long for the index
+        q.push({root, 0});  // Push the root node with index 0
+        int maxWidth = 0;   // Initialize maximum width
 
         while (!q.empty()) {
-            int size = q.size();
-            long long min = q.front().second;
-            long long first, last;
+            int n = q.size();  // Number of nodes at the current level
+            unsigned long long start = q.front().second;  // Start index of the current level
+            unsigned long long end = q.back().second;    // End index of the current level
 
-            for (int i = 0; i < size; i++) {
-                long long curr_id = q.front().second - min;
-                TreeNode* node = q.front().first;
+            // Calculate the width of the current level
+            int width = static_cast<int>(end - start + 1);  // Cast to int
+
+            // Update maxWidth if the current level is wider
+            maxWidth = max(maxWidth, width);
+
+            // Process nodes at the current level
+            for (int i = 0; i < n; i++) {
+                pair<TreeNode*, unsigned long long> p = q.front();
+                TreeNode* node = p.first;
+                unsigned long long idx = p.second;
                 q.pop();
 
-                if (i == 0)
-                    first = curr_id;
+                // If there's a left child, calculate its index and enqueue it
+                if (node->left) q.push({node->left, 2 * idx + 1});
 
-                if (i == size - 1)
-                    last = curr_id;
-
-                if (node->left) {
-                    q.push({node->left, curr_id * 2 + 1});
-                }
-
-                if (node->right) {
-                    q.push({node->right, curr_id * 2 + 2});
-                }
+                // If there's a right child, calculate its index and enqueue it
+                if (node->right) q.push({node->right, 2 * idx + 2});
             }
-
-            ans = max(ans, static_cast<int>(last - first + 1));
         }
 
-        return ans;
+        return maxWidth;  // Return the maximum width of the binary tree
     }
 };
