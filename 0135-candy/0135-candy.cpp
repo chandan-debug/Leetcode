0f94c7d092a2a06dabab02464b71ef314ab360b8
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-13 08:19:06 +0530

    Time: 16 ms (67.67%), Space: 17.7 MB (47.60%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
            int n = ratings.size();
-        std::vector<int> candies(n, 1);
+        vector<int> candies(n, 1);
 
         for (int i = 1; i < n; ++i) {
             if (ratings[i] > ratings[i - 1]) {
@@ -12,7 +12,7 @@ public:
 
         for (int i = n - 2; i >= 0; --i) {
             if (ratings[i] > ratings[i + 1]) {
-                candies[i] = std::max(candies[i], candies[i + 1] + 1);
+                candies[i] = max(candies[i], candies[i + 1] + 1);
             }
         }
 
