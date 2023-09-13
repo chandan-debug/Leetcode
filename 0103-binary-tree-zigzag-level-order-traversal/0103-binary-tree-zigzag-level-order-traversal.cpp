6e498afbc1afb3ddb0734c64fd8a1f155945bbda
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-13 13:31:39 +0530

    Time: 3 ms (57.40%), Space: 12 MB (64.26%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,33 +12,37 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==NULL)return ans;
-        queue<TreeNode*>q;
+        vector<vector<int>> ans;
+        if (root == NULL) {
+            return ans;
+        }
+
+        queue<TreeNode*> q;
         q.push(root);
-        bool lefttoright=true;
-        while(!q.empty()){
-           int size=q.size();
-            vector<int> row(size);
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
+        int level = 0;
+
+        while (!q.empty()) {
+            vector<int> level1;
+            int s = q.size();
+            
+            for (int i = 0; i < s; i++) {
+                TreeNode* node = q.front();
                 q.pop();
                 
-                int index=(lefttoright)?i:(size-1-i);
-                row[index]=node->val;
-                if(node->left){
-                    q.push(node->left);
+                if (level == 0) {
+                    level1.push_back(node->val);  // Push elements to the vector
+                } else {
+                    level1.insert(level1.begin(), node->val);  // Insert elements at the beginning
                 }
-                if(node->right){
-                    q.push(node->right);
-                }
+                
+                if (node->left != NULL) q.push(node->left);
+                if (node->right != NULL) q.push(node->right);
             }
-                lefttoright=!lefttoright;
-                ans.push_back(row);
             
-
+            ans.push_back(level1);
+            level = 1 - level;  // Toggle level for zigzag
         }
+        
         return ans;
-    
     }
 };
