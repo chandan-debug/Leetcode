30fad5a8c2eb2fd2f25e1cc41cb6c3b9f31c4561
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-14 12:26:57 +0530

    Time: 95 ms (5.20%), Space: 90.3 MB (5.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,55 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        vector<int> nums3(n + m);
-        int i = n - 1;
-        int j = m - 1;
-        int k = n + m - 1;
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int> merged;
 
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] < nums2[j]) {
-                nums3[k] = nums2[j];
-                j--;
+        if (m == 0 && n == 0) {
+            // Both input vectors are empty, return 0 as the median or handle as needed.
+            return 0.0;
+        } else if (m == 0) {
+            // If nums1 is empty, simply return the median of nums2.
+            int mid = n / 2;
+            if (n % 2 == 0) {
+                return static_cast<double>(nums2[mid - 1] + nums2[mid]) / 2.0;
             } else {
-                nums3[k] = nums1[i];
-                i--;
+                return static_cast<double>(nums2[mid]);
             }
-            k--;
-        }
-        while (i >= 0) {
-            nums3[k] = nums1[i];
-            i--;
-            k--;
-        }
-        while (j >= 0) {
-            nums3[k] = nums2[j];
-            j--;
-            k--;
+        } else if (n == 0) {
+            // If nums2 is empty, simply return the median of nums1.
+            int mid = m / 2;
+            if (m % 2 == 0) {
+                return static_cast<double>(nums1[mid - 1] + nums1[mid]) / 2.0;
+            } else {
+                return static_cast<double>(nums1[mid]);
+            }
         }
 
-        int mid = (n + m) / 2;
-        if ((n + m) % 2 == 0) {
-            return (nums3[mid - 1] + nums3[mid]) / 2.0;
+        for (int i = 0; i < m; ++i) {
+            if (nums1[i] > nums2[0]) {
+                // Swap elements if nums1[i] is greater than nums2[0]
+                swap(nums1[i], nums2[0]);
+
+                // Re-arrange nums2 to keep it sorted (assuming nums2 is already sorted)
+                int j = 0;
+                while (j < n - 1 && nums2[j] > nums2[j + 1]) {
+                    swap(nums2[j], nums2[j + 1]);
+                    ++j;
+                }
+            }
+        }
+
+        // Merge nums1 and sorted nums2
+        merged.insert(merged.end(), nums1.begin(), nums1.end());
+        merged.insert(merged.end(), nums2.begin(), nums2.end());
+
+        int totalSize = merged.size();
+        int mid = totalSize / 2;
+        if (totalSize % 2 == 0) {
+            return static_cast<double>(merged[mid - 1] + merged[mid]) / 2.0;
         } else {
-            return nums3[mid];
+            return static_cast<double>(merged[mid]);
         }
     }
 };
