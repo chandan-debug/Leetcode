f92faca99e842e63a046aebed274575b05c89733
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-09-14 11:09:47 +0530

    Time: 27 ms (63.38%), Space: 90 MB (29.28%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums2.size() < nums1.size())
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        int low = 0, high = n1;
-        
-        while (low <= high) {
-            int cut1 = (low + high) >> 1;
-            int cut2 = ((n1 + n2 + 1) / 2) - cut1;
-            
-            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1 - 1];
-            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2 - 1];
-            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
-            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
-            
-            if (left1 <= right2 && left2 <= right1) {
-                if ((n1 + n2) % 2 == 0) {
-                      return  (max(left1, left2) + min(right1, right2)) / 2.0;
-                   
-                } else {
-                    return max(left1, left2);
-                }
-            } else if (left1 > right2) {
-                high = cut1 - 1;
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<int> nums3(n + m);
+        int i = n - 1;
+        int j = m - 1;
+        int k = n + m - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] < nums2[j]) {
+                nums3[k] = nums2[j];
+                j--;
             } else {
-                low = cut1 + 1;
+                nums3[k] = nums1[i];
+                i--;
             }
+            k--;
         }
-    
-        return 0.0; // Default return value if no median is found
+        while (i >= 0) {
+            nums3[k] = nums1[i];
+            i--;
+            k--;
+        }
+        while (j >= 0) {
+            nums3[k] = nums2[j];
+            j--;
+            k--;
+        }
+
+        int mid = (n + m) / 2;
+        if ((n + m) % 2 == 0) {
+            return (nums3[mid - 1] + nums3[mid]) / 2.0;
+        } else {
+            return nums3[mid];
+        }
     }
 };
-
