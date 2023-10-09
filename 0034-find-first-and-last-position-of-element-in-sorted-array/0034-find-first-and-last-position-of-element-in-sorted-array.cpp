025b5b1ad81e8d31ddf9537a53861e51948bab53
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-10-09 10:58:50 +0530

    Time: 10 ms (18.89%), Space: 14.1 MB (8.57%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,14 +1,16 @@
 class Solution {
-    public:
-    int firstsearch(vector<int>& nums,int target,int l,int h){
+public:
+    int firstsearch(vector<int>& nums, int target,int l ,int h){
         int start=-1;
+        
         while(l<=h){
             int mid=(l+h)>>1;
+            
             if(nums[mid]==target){
                 start=mid;
                 h=mid-1;
             }
-            if(target>nums[mid]){
+             if(target>nums[mid]){
                 l=mid+1;
             }
             if(target<nums[mid]){
@@ -17,29 +19,30 @@ class Solution {
         }
         return start;
     }
-     int lastsearch(vector<int>& nums,int target,int l,int h){
+      int lastsearch(vector<int>& nums, int target,int l ,int h){
         int end=-1;
+        
         while(l<=h){
             int mid=(l+h)>>1;
+            
             if(nums[mid]==target){
                 end=mid;
                 l=mid+1;
             }
-            if(target>nums[mid]){
+             if(target>nums[mid]){
                 l=mid+1;
             }
             if(target<nums[mid]){
-                h=mid-1;
-            }
+                h=mid-1;}
         }
         return end;
+        
     }
-public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int start=firstsearch(nums,target,0,nums.size()-1);
-       int end=lastsearch(nums,target,0,nums.size()-1);
-        return {start,end};
-
+        int end=lastsearch(nums,target,0,nums.size()-1);
+        
+        return{start,end};
         
         
     }
