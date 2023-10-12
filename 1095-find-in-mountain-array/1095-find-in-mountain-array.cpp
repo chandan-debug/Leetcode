67f0effc538b930bbd7a200679cb9be89ca0313f
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-10-12 15:29:50 +0530

    Time: 6 ms (44.79%), Space: 7.3 MB (27.99%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,62 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mArray) {
+        int n = mArray.length();
+        
+        // Binary search to find the peak element
+        int left = 0, right = n - 1;
+        
+        while (left < right) 
+        {
+            int mid = left + (right - left) / 2;
+            int midValue = mArray.get(mid);
+            int nextValue = mArray.get(mid + 1);
+            
+            if (midValue < nextValue) left = mid + 1;
+            else  right = mid;
+        }
+        
+        int peak = left;  // Peak index
+        
+        // Search in the left side of the peak
+        left = 0;
+        right = peak;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = mArray.get(mid);
+            
+            if (midValue == target) return mid;
+            else if (midValue < target) left = mid + 1;
+            else right = mid - 1;
+            
+        }
+        
+        // Search in the right side of the peak
+        left = peak;
+        right = n - 1;
+        
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+            int midValue = mArray.get(mid);
+            
+            if (midValue == target) return mid;
+            else if (midValue < target) right = mid - 1;
+            else left = mid + 1;
+            
+        }
+        
+        return -1;  // Target not found in the MountainArray
+    }
+};
