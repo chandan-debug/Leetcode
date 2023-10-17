9347b3635d449f10039bcd48bb1e504ad32e287d
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-10-17 20:48:13 +0530

    Time: 73 ms (92.10%), Space: 68.2 MB (15.78%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,13 +4,13 @@ public:
         int maxi=INT_MIN;
           int sum=0;
         for(int i=0;i<nums.size();i++){
-                sum+=nums[i];
-                maxi=max(sum,maxi);
+
+            sum+=nums[i];
+             maxi=max(maxi,sum) ; 
             if(sum<0)
                 sum=0;
-        
+               
         }
-        return maxi;
-        
+  return maxi;
     }
 };
