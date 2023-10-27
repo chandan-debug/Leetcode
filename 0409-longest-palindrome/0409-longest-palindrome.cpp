4949698f5bdbef78950f26a97d2bf8bf4f586dab
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-10-27 11:55:27 +0530

    Time: 0 ms (100.00%), Space: 7 MB (26.12%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> frequency;
+        unordered_map<char,int> mp;
         
-        // Count the frequency of each character in the string.
-        for (char c : s) {
-            frequency[c]++;
-        }
-        
-        int length = 0;
-        bool hasOddFrequency = false;
-
-        // Iterate through the frequency map.
-        for (const auto& pair : frequency) {
-            int count = pair.second;
+        for(char c:s){
+            mp[c]++;
             
-            // If the frequency is even, we can use all of them in the palindrome.
-            if (count % 2 == 0) {
-                length += count;
-            } else {
-                // If the frequency is odd, we can use all even occurrences and one odd occurrence.
-                length += count - 1;
-                hasOddFrequency = true;
+        }
+        int len=0;
+        bool odd=false;
+        
+        for(const auto& x:mp){
+            int cnt=x.second;
+            
+            if(cnt%2==0){
+                len+=cnt;
+            }
+            else{
+                len+=cnt-1;
+                odd=true;
             }
         }
         
-        // If there was at least one character with an odd frequency, add 1 to the length.
-        if (hasOddFrequency) {
-            length++;
+        if(odd){
+            len++;
         }
-        
-        return length;
+        return len;
     }
 };
