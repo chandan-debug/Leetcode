8336d4e39eab46fa43af98b1e3268ccbfe122397
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-10-28 13:01:46 +0530

    Time: 3 ms (70.77%), Space: 9.7 MB (20.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
         if(strs.empty()){
             return " ";
-            
         }
         
         sort(strs.begin(),strs.end());
         string first=strs[0];
-        string last=strs[strs.size()-1];
-        string ans;
+       string  last=strs[strs.size()-1];
+        string ans="";
+        
         for(int i=0;i<first.size();i++){
             if(first[i]!=last[i]){
                 break;
             }
-            ans.push_back(first[i]);
+            else{
+                ans.push_back(first[i]);
+            }
         }
         return ans;
     }
