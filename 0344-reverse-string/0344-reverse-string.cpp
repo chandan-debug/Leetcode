a0685391474323eef75aa394a749c87e19f7a65b
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-10-30 00:29:19 +0530

    Time: 19 ms (51.79%), Space: 23.5 MB (67.16%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int left = 0;
-        int right = s.size() - 1;
-        
-        while (left < right) {
-            swap(s[left], s[right]);
-            left++;
-            right--;
+        int left=0;
+        int right=s.size()-1;
+        while(left<=right){
+            swap(s[left++],s[right--]);
         }
+        
     }
-};;
-// In this code, we're using a two-pointer approach to swap characters from the left and right ends of the vector until they meet in the middle, effectively reversing the string in-place.
-
-
-
-
-
+};
