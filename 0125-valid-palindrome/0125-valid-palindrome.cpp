7b356ae670f93ecb250f58cb1ca40bdf69abff43
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-11-04 20:30:09 +0530

    Time: 6 ms (44.69%), Space: 7.9 MB (33.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // Clean the string by removing non-alphanumeric characters and converting to lowercase
         string ans;
-        for (char c : s) {
-            if (isalnum(c)) {
-                ans += tolower(c);
+        for(auto c:s){
+            if(isalnum(c)){
+                ans+=tolower(c);
             }
         }
-
-        // Check if the cleaned string is a palindrome
-        int left = 0;
-        int right = ans.size() - 1;
-        while (left < right) {
-            if (ans[left] != ans[right]) {
-                return false;
+            int left=0;
+            int right=ans.size()-1;
+            
+            while(left<right){
+                if(ans[left]!=ans[right]){
+                    return false;
+                }
+                left++;
+                right--;
             }
-            left++;
-            right--;
+            return true;  
         }
-        return true;
-    }
+      
 };
