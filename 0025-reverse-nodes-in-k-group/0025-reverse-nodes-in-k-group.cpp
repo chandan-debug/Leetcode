32096b9fe8cdbee711d214fbbc97dc440fe4fc87
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-11-27 15:20:11 +0530

    Time: 7 ms (91.90%), Space: 11.9 MB (38.97%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -11,30 +11,35 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL || k==1)
+        ListNode* curr=head;
+        ListNode*prev=NULL;
+        ListNode* next=NULL;
+        
+        int count=0;
+        ListNode* temp=head;
+        
+        while(temp && count<k){
+            temp=temp->next;
+            count++;
+        }
+        
+        if(count<k){
             return head;
-        ListNode* dummy=new ListNode(0);
-        dummy->next=head;
-        
-        ListNode* curr=dummy, *nex=dummy, *pre=dummy;
-        int cnt=0;
-        while(curr->next !=NULL){
-            curr=curr->next;
-            cnt++;
         }
         
-        while(cnt>=k){
-            curr=pre->next;
-            nex=curr->next;
-            for(int i=1;i<k;i++){
-                curr->next=nex->next;
-                nex->next=pre->next;
-                pre->next=nex;
-                nex=curr->next;
-            }
-            pre=curr;
-            cnt-=k;
+        count=0;
+        while(curr!=NULL && count<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            count++;
         }
-        return dummy->next;
+        
+        if(next!=NULL){
+            head->next=reverseKGroup(next,k);
+        }
+        
+        return prev;
     }
 };
