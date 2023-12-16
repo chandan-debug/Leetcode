7e4b93b822f5e7c022a3d26f80aad96183c73404
chandan-debug <91668367+chandan-debug@users.noreply.github.com>
2023-12-16 13:24:30 +0530

    Time: 5 ms (82.11%), Space: 8 MB (9.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
+        if(s.size()!=t.size()){
             return false;
         }
+        
         vector<int> count(256,0);
-        for(char c:s) {
+        for(char c:s){
             count[c]++;
         }
-        for(char c:t) {
-            if(count[c]==0) return false;
+        
+        for(char c:t){
+            if(count[c]==0)
+                return false;
             count[c]--;
         }
         return true;
